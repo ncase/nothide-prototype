b5f9ed83317f65fc55598c7392a2bfaa7ab6e937
Nicklaus Liow <nicklaus.liow@gmail.com>
2013-10-29 13:08:49 -0700

    heroku fix fix

---
+++ README.md
@@ -0,0 +1,4 @@
+# Nothing To Hide: POLISHED PROTOTYPE
+
+Just a polished prototype. This ain't even an alpha or even a demo.    
+Enjoy!
--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 var connect = require('connect');
-connect.createServer(connect.static(__dirname)).listen(process.env.PORT);
+connect.createServer(connect.static(__dirname+'/game')).listen(process.env.PORT);
+console.log("Listening on port "+process.env.PORT);
